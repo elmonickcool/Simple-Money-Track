9e0023281490080c09d2b7effd19c65092ffb2d0
elmonickcool <elmonickol@gmail.com>
2024-09-21 19:33:38 +0800

    create index page

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+    
+    <h1>Money Track</h1>
+    
+    <hr>
+    <h2>Dashboard</h2>
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card bg-success text-white">
+                <div class="card-body">
+                    <h5 class="card-title">Income</h5>
+                    <p class="card-text">$1,000</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card bg-danger text-white">
+                <div class="card-body">
+                    <h5 class="card-title">Expenses</h5>
+                    <p class="card-text">$500</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card bg-info text-white">
+                <div class="card-body">
+                    <h5 class="card-title">Balance</h5>
+                    <p class="card-text">$500</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    Latest Transactions
+    
+    </div>
+</body>
+
+</html>
